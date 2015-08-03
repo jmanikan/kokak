d3fa7589ea3cfe3b6dbe0f6e8c53bf20da2e2dd0
Juvenal Manikan <jmanikan@gmail.com>
2015-08-02 23:08:25 -0400

    Created user relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,11 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true, presence: true
   #format: { with: /^[\w.+-]+@([\w]+.)+\w+$/ }
   has_many :ribbits
+  has_many :follower_relationships, classname: "Relationship", foreign_key: "followed_id"
+  has_many :followed_relationships, classname: "Relationship", foreign_key: "follower_id"
+
+  has_many :followers, through: :follower_relationships
+  has_many :followeds, through: :followed_relationships
 
   private # All following methods cannot be accessed outside the class (on instances)
 
@@ -24,5 +29,13 @@ class User < ActiveRecord::Base
 
   def create_avatar_url
     self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
+  end
+
+  def following? user
+    self.followeds.include? user
+  end
+
+  def follow user
+      Relationship.create follower_id: self.id, followed_id: user.id
   end
 end
